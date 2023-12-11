c31897aad9ac09dff10531e71c09e78f8d30f0ff
mailach <alina.mailach@informatik.uni-leipzig.de>
2023-12-11 19:41:01 +0100

    Receive create-question via websocket

--- backend/app/admin/events.py
+++ backend/app/admin/events.py
@@ -1,9 +1,11 @@
+import logging
 from flask_socketio import emit
 from .. import socketio
 
 
-@socketio.on('test', namespace='/admin')
+@socketio.on('question-create', namespace='/admin')
 def test(message):
+    logging.error(message)
     emit('status', {'msg': 'Yep, admin test successful'})
 
 
