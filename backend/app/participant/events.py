b31b413366bd200f9da9432dd4fa306b7d4f98ad
mailach <alina.mailach@informatik.uni-leipzig.de>
2024-03-31 10:42:08 +0200

    Add team registration basic

--- backend/app/participant/events.py
+++ backend/app/participant/events.py
@@ -1,5 +1,6 @@
 import logging
 from flask_socketio import emit
+from flask import request
 from .. import socketio, kafka_producer
 
 
@@ -7,6 +8,10 @@ from .. import socketio, kafka_producer
 @socketio.on('register', namespace='/register')
 def get_questions(message):
     logging.error(message)
+    print(request.sid)
+    kafka_producer.send("teams", key=message, value={"id": message, "points": 0, "activated": 0, "client_id":request.sid })
+    emit("registration", {"msg": "Specific to use"}, room=request.sid)
+
     emit('registration', {"msg": "successful"})
 
 
